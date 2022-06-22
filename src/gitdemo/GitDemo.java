9fba498281f16f8057aa4f34c8e3e967d2d779f9
whatfsg <edwinattukaran11@gmail.com>
2022-06-22 01:21:27 -0400

    New repo for student code

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -14,6 +14,7 @@ public class GitDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Whatsup");
         // TODO code application logic here
     }
     
